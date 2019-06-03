93a6ea3b62148c2a8135351d65a56d4da90e3513
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-03 13:09:43 -0400

    Adds another frotz() example for O(n^2) runtime complexity

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,10 +6,33 @@ def foo(n):
         print(i)                  # O(1)
   
 # O(1) + O(sqrt n) choose dominant term
-
 # O(sqrt n)
 
-print(foo(23))
+print(foo(36))
+
+def frotz(n):
+    s = 0  # O(1)
+
+    for i in range(n):  # O(n)  
+        for j in range(2 * n):  # O(2 * n)
+            s += i * j  # O(1)
+    
+    return s  # O(1)
+
+print(frotz(36))
+
+# 1 + (n * ((2 * n)) * 1)
+# 1 + 2 * n * n * 1
+# 1 + 2 * n^2
+# 2 * n^2
+
+# O(2 * n^2) <-- drop the constant
+# O(n^2) <-- final runtime complexity
+
+          
+
+
+
 
 # example_list = [1, 4, 2, 7, 9, 5, 3, 6, 2, 8, 10]
 
