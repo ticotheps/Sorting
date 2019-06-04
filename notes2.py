1360978daa1110f873a13628d4b1e74969f5f595
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-04 13:33:13 -0400

    Adds another example to notes2.py

--- notes2.py
+++ notes2.py
@@ -33,7 +33,7 @@
 
 #----------------------------------------------------------
 
-# Example 3 (pseudocode)
+# Example 3
 # Space Complexity: 
 # Runtime Complexity:
 # sum(a, b) === a + sum(a+1, b)
@@ -47,13 +47,47 @@
 # b == 1
 #-------
 
-def sum(a, b):  # O(n) === O(b-a)
-    if a == b:  # Base Cases are always in the form of a conditional
-        return 0
+# def sum(a, b):  # O(n) === O(b-a)
+#     if a == b:  # Base Cases are always in the form of a conditional
+#         return 0
 
-    return a + sum(a+1, b)
+#     return a + sum(a+1, b)
 
-x = sum(0, 4)
-#  runs "0 + 1 + 2 + 3 + 0"
+# x = sum(0, 4)
+# #  runs "0 + 1 + 2 + 3 + 0"
 
-print(x)
+# print(x)
+
+#----------------------------------------------------------
+
+# Example 4
+# Space Complexity: 
+# Runtime Complexity: O(2^n)
+counter = 0
+
+def foo(n):
+    global counter
+
+    if n <= 0:
+        return
+    
+    # print(n)
+    counter += 1
+
+    # Each call makes two calls, so O(2^n)
+    # If each call makes 3 calls, then O(3^n)
+    foo(n-1)
+    foo(n-2)
+
+# foo(1)    # 1
+# foo(2)    # 2
+# foo(3)    # 4
+foo(4)    # 7
+# foo(5)    # 12
+# foo(6)      # 20
+
+# O(2^n)
+
+print(counter)
+
+#----------------------------------------------------------
